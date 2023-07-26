3ea648e2020e6ef7ced9593385ef5f52a2e75b90
RedouaneJnah98 <redouanejnah2000@gmail.com>
2023-07-26 13:27:46 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Jnah Redouane  on 26/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
