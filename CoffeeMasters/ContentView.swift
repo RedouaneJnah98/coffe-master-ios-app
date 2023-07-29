c14a139ef933b92e1e9b08cfff11f3cf4f00aadb
RedouaneJnah98 <redouanejnah2000@gmail.com>
2023-07-29 23:38:09 +0100

    First Commit

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = "";
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          TextField("Enter your name", text: $name)
+          Text("Hello \(name)")
         }
-        .padding()
     }
 }
 
